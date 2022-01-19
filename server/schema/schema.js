a09fa6c574d8e29d4eee4b2c926e9eb51c0bb2a6
Khalid-MK <khalidabdelnaser382@gmail.com>
2022-01-19 15:20:57 +0200

    .

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,13 +1,20 @@
 const graphql = require('graphql');
 const _ = require('lodash');
 
-const { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLID } = graphql;
+const { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLID } = graphql;
 
 const movies = [
     { name: "Joker", genre: "Drama", id: "1" },
     { name: "Batman", genre: "Action", id: "2" },
     { name: "La La Land", genre: "Musical", id: "3" },
     { name: "Interstellar", genre: "Sci-Fi", id: "4" },
+]
+
+const directories = [
+    { name: "Tod Philips", age: 60, id: "1" },
+    { name: "Wes Anderson", age: 52, id: "2" },
+    { name: "Damien Chazele", age: 58, id: "1" },
+    { name: "Chirstopher Nolan", age: 51, id: "1" },
 ]
 
 const MovieType = new GraphQLObjectType({
@@ -17,6 +24,17 @@ const MovieType = new GraphQLObjectType({
             id: { type: GraphQLID },
             name: { type: GraphQLString },
             genre: { type: GraphQLString },
+        }
+    }
+})
+
+const DirectorType = new GraphQLObjectType({
+    name: 'Director',
+    fields: () => {
+        return {
+            id: { type: GraphQLID },
+            name: { type: GraphQLString },
+            age: { type: GraphQLInt },
         }
     }
 })
@@ -34,6 +52,15 @@ const RootQuery = new GraphQLObjectType({
                     // get data from mongo
                     // mock using lodash
                     return _.find(movies, { id: args.id });
+                }
+            },
+            director: {
+                type: DirectorType,
+                args: {
+                    id: { type: GraphQLID }
+                },
+                resolve(parent, args) {
+                    return _.find(directories, { id: args.id });
                 }
             }
         }
